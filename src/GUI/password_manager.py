6aef16488ce37f042afe7d6ad426a828fdd5511f
andad <andadeacu@yahoo.com>
2023-04-14 20:44:46 +0300

    learn GUI with tkinter

---
+++ src/GUI/password_manager.py
@@ -0,0 +1,6 @@
+from tkinter import *
+
+window = Tk()
+window.title("Password Manager App")
+
+window.mainloop()
---
+++ src/GUI/pomodoro.py
@@ -0,0 +1,6 @@
+from tkinter import *
+
+window = Tk()
+window.title("Pomodoro App")
+
+window.mainloop()
